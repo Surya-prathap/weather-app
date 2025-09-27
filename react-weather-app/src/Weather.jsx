43dc8f27d2f871245fd562aab1d00a0e0bb6370a
Surya Prathap Reddy <13794prathap@gmail.com>
2025-09-27 12:56:47 +0530

    adding css to weather app

--- react-weather-app/src/Weather.jsx
+++ react-weather-app/src/Weather.jsx
@@ -33,7 +33,7 @@ const Weather = () => {
       });
     } catch (error) {
       setWeatherData(false);
-      console.error("Error in fetching weather data");
+      alert("Error in fetching weather data");
     }
   };
 
@@ -41,22 +41,51 @@ const Weather = () => {
     search("delhi");
   }, []);
   return (
-    <div>
-      <div>
+    <div className="container">
+      <h1 className="title">WEATHER APP</h1>
+      <div className="search-info">
         <input ref={inputRef} placeholder="Enter City....." />
-        <button onClick={() => search(inputRef.current.value)}>search</button>
+        <button
+          className="search-btn"
+          onClick={() => search(inputRef.current.value)}
+        >
+          search
+        </button>
       </div>
       {weatherData ? (
         <>
           <div>
-            <h2>{weatherData.name}</h2>
-            <h3>Temperature: {weatherData.temp}&deg;C </h3>
-          </div>
-          <div>
-            <h3>Location Latitude : {weatherData.lat}&deg;N </h3>
-            <h3>Location Longitude : {weatherData.lon}&deg;E</h3>
-            <h3>Wind speed : {weatherData.speed} km/h</h3>
-            <h3>Humidity : {weatherData.humidity}%</h3>
+            <div className="info">
+              <h2 className="name">{weatherData.name.toUpperCase()}</h2>
+
+              <div className="temp">
+                <img
+                  className="temp-logo"
+                  src={
+                    weatherData.temp > 30
+                      ? "./src/assets/hot-climate.png"
+                      : "./src/assets/cold-climate.png"
+                  }
+                ></img>
+                <h3>Temperature: {weatherData.temp}&deg;C </h3>
+              </div>
+
+              <div className="lat-lon">
+                <h3>Location Latitude : {weatherData.lat.toFixed(2)}&deg;N </h3>
+
+                <h3>Location Longitude : {weatherData.lon.toFixed(2)}&deg;E</h3>
+              </div>
+            </div>
+            <div className="speed-humidity-container">
+              <div className="speed">
+                <img className="logo" src="./src/assets/wind-logo.png"></img>
+                <h3>Wind speed : {weatherData.speed} km/h</h3>
+              </div>
+              <div className="humidity">
+                <img className="logo" src="./src/assets/humidity.png"></img>
+                <h3>Humidity : {weatherData.humidity}%</h3>
+              </div>
+            </div>
           </div>
         </>
       ) : (
